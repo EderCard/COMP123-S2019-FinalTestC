865481238f30f4d4a3dc18f3742b72f9f78f5934
EderCard <edersoncard@gmail.com>
2019-08-12 15:30:11 -0400

    Added tab control.

--- COMP123-S2019-FinalTestC/Views/CharacterGenerationForm.cs
+++ COMP123-S2019-FinalTestC/Views/CharacterGenerationForm.cs
@@ -5,7 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-
+/*
+ * Student Name: Ederson Cardoso
+ * Student ID: 301033332
+ * Description: This is the main Form for the application
+ */
 namespace COMP123_S2019_FinalTestC.Views
 {
     public partial class CharacterGenerationForm : COMP123_S2019_FinalTestC.Views.MasterForm
@@ -13,6 +17,31 @@ namespace COMP123_S2019_FinalTestC.Views
         public CharacterGenerationForm()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// This is the event handler for the Back Button clik event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            if (MainTabControl.SelectedIndex != 0)
+            {
+                MainTabControl.SelectedIndex--;
+            }
+        }
+        /// <summary>
+        /// This is the event handler for the Next Button clik event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            if (MainTabControl.SelectedIndex < MainTabControl.TabPages.Count -1)
+            {
+                MainTabControl.SelectedIndex++;
+
+            }
         }
     }
 }
